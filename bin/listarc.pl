0b2fbec042f802a7ca33424d5f92c677e51d3dbf
carlos.pantelides <carlos.pantelides@gmail.com>
2011-10-02 14:08:50 +0000

    listarc: carga de maestros en memoria

--- bin/listarc.pl
+++ bin/listarc.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl -w
 
+use strict;
+use warnings;
+
+use Util;
+use Lib;
+
 # Los datos que hacen falta de la configuracion son:
 #    ~/grupo2/mae
 #    ~/grupo2/ya/encuestas.sum
@@ -8,97 +14,27 @@
 # conocidos programaticamente, ya que la configuracion se halla en ~/grupo2/conf
 
 # $raiz="~/grupo2";
-$raiz="/home/carlos/7508/svn/trunk";
+my $raiz="/home/carlos/7508/7508fiuba2011g2/trunk";
 
-$suma_encuestas="$raiz/ya/encuestas.sum";
-$maestro_encuestadores="$raiz/mae/encuestadores.mae";
-$maestro_encuestas="$raiz/mae/encuestas.mae";
-$maestro_preguntas="$raiz/mae/preguntas.mae";
-
-# tomado de 
-# http://stackoverflow.com/questions/2363142/how-to-iterate-through-hash-of-hashes-in-perl
-sub hash_walk {
-    my ($hash, $key_list, $callback) = @_;
-    while (my ($k, $v) = each %$hash) {
-        # Keep track of the hierarchy of keys, in case
-        # our callback needs it.
-        push @$key_list, $k;
-
-        if (ref($v) eq 'HASH') {
-            # Recurse.
-            hash_walk($v, $key_list, $callback);
-        }
-        else {
-            # Otherwise, invoke our callback, passing it
-            # the current key and value, along with the
-            # full parentage of that key.
-            $callback->($k, $v, $key_list);
-        }
-
-        pop @$key_list;
-    }
-}
+my $suma_encuestas="$raiz/ya/encuestas.sum";
+my $maestro_encuestadores="$raiz/mae/encuestadores.mae";
+my $maestro_encuestas="$raiz/mae/encuestas.mae";
+my $maestro_preguntas="$raiz/mae/preguntas.mae";
 
 
-sub print_hash {
-    my ($hash) = @_;
-    print "PRINT HASH\n";
-    while (my ($key, $value) = each %$hash) {
-        print ".\n";
-        if ('HASH' eq ref $value) {
-            print "HASH";
-            print_hash $value;
-        } else {
-            print "VALUE";
-            #$fn->($value);
-            print $value;
-        }
-    }
-}
+my %encuestas = Lib::cargar_encuestas($maestro_encuestas);
+Util::imprimir_lista(\%encuestas);
 
-#
-#
-#
-sub cargar_encuestas {
-    open(ARCHIVO,$maestro_encuestas) || die ("No se pudo cargar encuestas: $! $maestro_encuestas");
-    while (<ARCHIVO>) {
-        chomp;
-        ( 
-            $encuesta{"codigo"},
-            $encuesta{"nombre"},
-            $encuesta{"cantidad"},
-            $encuesta{"rojo_inicio"},
-            $encuesta{"rojo_fin"},
-            $encuesta{"amarillo_inicio"},
-            $encuesta{"amarillo_fin"},
-            $encuesta{"verde_inicio"},
-            $encuesta{"verde_fin"}
-        ) = split(/,/);
+my %encuestadores = Lib::cargar_encuestadores($maestro_encuestadores);
+Util::imprimir_lista(\%encuestadores);
 
-        $lista_encuestas{$encuesta{"codigo"}}=$encuesta;
-        print_hash $encuesta;
-    }
-    close(ARCHIVO);
-    
-    print_hash $lista_encuestas;
+my %preguntas = Lib::cargar_preguntas($maestro_preguntas);
+Util::imprimir_lista(\%preguntas);
 
-    return (%lista_encuestas);
-}
+my %suma = Lib::cargar_suma($suma_encuestas);
+Util::imprimir_lista(\%suma);
 
-%encuestas = cargar_encuestas();
-# foreach $encuesta (cargar_encuestas()) {
-#   print $encuesta{"codigo"};
-#   foreach $campo (%encuesta) {
-#     print "$campo, ";
-#   }
-#   print "\n";
-# }
 
-#%encuestas=;
-
-#%encuestadores=;
-
-#%preguntas=;
 #
 #
 #
