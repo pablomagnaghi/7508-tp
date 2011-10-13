c65aa0976e4f6e2854da2c8a2c47759e200beeaa
maxig88@gmail.com <maxig88@gmail.com@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-13 06:55:46 +0000

    sumarC.sh funcionando, probablemente le falte testing. Faltan algunos detalles, como acomodar la estructura de los directorios, usar bien el logger y mover los archivos procesados, pero lo mas pesado ya está hecho.

--- trunk/bin/sumarC.sh
+++ trunk/bin/sumarC.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+#======================================================================================
+# AUTOR: Maximiliano Gismondi
+#
+# ARCHIVO: sumarC.sh
+#
+# USO: sumarC.sh
+#======================================================================================
 
-paramsError=1
-pregIdError=18
-encuestaIdError=19
 
-origIfs=$IFS
-IFS=$'\n'
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    
+#
+# PRE-CONDICIÓN:  
+# POST-CONDICIÓN: 
+#
+# PARÁMETRO 1:    Tipo.
+# PARÁMETRO 2:    Mensaje.
+# 
+# CÓDIGO ERROR:   0: Éxito
+#======================================================================================
+logear() {
+	echo "$1 - $2" >> "archivoDeLog" # TODO Hacer bien
+	return 0
+}
 
-grupo=.
-preguntas=($(cat "$grupo/mae/preguntas.mae"))
-encuestas=($(cat "$grupo/mae/encuestas.mae"))
-archSumario="$grupo/grupo2/ya/encuestas.sum"
-archEncuestasRech="$grupo/grupo2/nolistos/encuestas.rech"
-sc=,												# Separador de campos
-
-: > "$archSumario" # TODO borrar
-: > "$archEncuestasRech" # TODO borrar
-: > "unArchivo" # TODO borrar
-
-# Recibe un registro de tipo pregunta, devuelve la ponderacion
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Parsea un registro pregunta ( del archivo 'preguntas.mae' ) para
+#                 obtener el factor de ponderacion.
+#
+# PRE-CONDICIÓN:  El registro 'pregunta' que recibe esta correctamente formateado según 
+#                 el enunciado del TP.
+# POST-CONDICIÓN: Emite por stdout el factor numérico de ponderacion. Ej "-2","+1",etc
+#
+# PARÁMETRO 1:    Registro 'pregunta'.
+# 
+# CÓDIGO ERROR:   0: Éxito
+#======================================================================================
 obtenerPonderacion() {
 	tipoPregunta=`echo $1 | cut -d $sc -f 3`
 	ponderacion=`echo $1 | cut -d $sc -f 4`
@@ -32,10 +49,22 @@ obtenerPonderacion() {
 	return 0
 }
 
-# Recibe id de pregunta y respuesta, devuelve el puntaje calculado
-# Errores: id de pregunta no encontrado
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Calcula el puntaje de una respuesta
+#
+# PRE-CONDICIÓN:  La variable 'preguntas' contiene las preguntas de el archivo maestro
+#                 'preguntas.mae'
+# POST-CONDICIÓN: Emite por stdout el puntaje de la respuesta
+#
+# PARÁMETRO 1:    Id de pregunta.
+# PARÁMETRO 2:    Valor de la respuesta.
+# 
+# CÓDIGO ERROR:   0: Éxito
+#                 $paramsError: Parámetro 1 o 2 nulo.
+#                 $pregIdError: Id de pregunta no encontrado (i.e pregunta inexistente)
+#======================================================================================
 obtenerPuntaje() {
-	if [ -z "$1" ] ; then 
+	if [ -z "$1" -o -z "$2" ] ; then 
 		return $paramsError
 	fi
 
@@ -59,8 +88,20 @@ obtenerPuntaje() {
 	return 0
 }
 
-# Recibe codigo de encuesta, devuelve cantidad de preguntas para esa encuesta
-# Errores: id de encuesta no encontrado
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Obtiene la cantidad de preguntas de una encuesta determinada
+#
+# PRE-CONDICIÓN:  La variable 'encuestas' contiene las preguntas de el archivo maestro
+#                 'encuestas.mae'
+# POST-CONDICIÓN: Emite por stdout la cantidad de preguntas para la encuesta que se
+#                 corresponde con el id recibido como parametro 
+#
+# PARÁMETRO 1:    Id de encuesta.
+# 
+# CÓDIGO ERROR:   0: Éxito
+#                 $paramsError - Parámetro 1 nulo
+#                 $encuestaIdError - Id de encuesta no encontrado
+#======================================================================================
 obtenerCantPreguntas() {
 	if [ -z "$1" ] ; then 
 		return $paramsError
@@ -82,37 +123,140 @@ obtenerCantPreguntas() {
 	return 0
 }
 
-# Recibe el numero de linea del bloque cabecera y el nombre de una variable. 
-# Emite el bloque y guarda en la variable el numero de linea del siguiente bloque
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Imprime un bloque de encuesta al archivo de encuestas rechazadas.
+#
+# PRE-CONDICIÓN:  La variable 'lineas' contiene las preguntas de el archivo de
+#                 encuestas que se esta procesando.
+# POST-CONDICIÓN: Emite por stdout el bloque Cabecera-n*Detalles que se quiere 
+#                 desechar, comenzando por la linea que se recibe como primer parámetro
+#                 hasta llegar a una linea que no cumpla con la expresión regular
+#                 almacenada en la variable 'regexDetalleSimple'
+#                 Almacena en la variable ${!$2} el valor de la primer línea del 
+#                 siguiente bloque.
+#
+# PARÁMETRO 1:    Número de la primer línea del bloque.
+# PARÁMETRO 2:    Nombre de una variable en donde se almacenara el numero de línea del
+#                 siguiente bloque.
+# 
+# CÓDIGO ERROR:   0: Éxito.
+#======================================================================================
 desecharBloque() {	
 	aux="$1"
 	ptrNroLineaSigBloque="$2"
-	while [ "$aux" -le "${#lineas[@]}" -a `echo "${lineas[$aux]}" | grep -E -c "^D.*$"` -eq "1" ] ; do
+	
+	if [ `echo "${lineas[$aux]}" | grep -E -c "^C.*$"` -eq "1" ]; then
+		echo "${lineas[$aux]}"
+		let "aux += 1"
+	fi
+
+	while [ "$aux" -le "${#lineas[@]}" -a \
+            `echo "${lineas[$aux]}" | grep -E -c "$regexDetalleSimple"` -eq "1" ] ; do
 		echo "${lineas[$aux]}"
 		let "aux += 1"
 	done
-	eval $ptrNroLineaSigBloque="$aux"	
+	eval $ptrNroLineaSigBloque="$aux"
+	return 0
 }
 
-# validarCantidadPreguntas <primer registro de detalle> <cantidad de preguntas>
-# devuelve 0 si coinciden, >0 si hay mas preguntas, <0 si hay menos
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Valida que el userId se corresponda con un identificador de un
+#                 encuestador válido.  
+#
+# PRE-CONDICIÓN:  La variable 'encuestadores' contiene la información sobre los 
+#                 encuestadores almacenada en el archivo maestro de encuestadores 
+#                 'encuestadores.mae'.
+#
+# POST-CONDICIÓN: ---
+#
+# PARÁMETRO 1:    Id de encuestador.
+# 
+# CÓDIGO ERROR:   0: El id de encuestador existe en el archivo maestro.
+#                 $encuestadorIdError: El id de encuestador no fue encontrado.
+#======================================================================================
+validarUserId() {
+	aux="0"
+	while [ "$aux" -le "${#encuestadores[@]}" -a "$userIdActual" != "$1" ] ; do
+		userIdActual=`echo "${encuestadores[$aux]}" | cut -d "${sc}" -f 3`
+		let "aux += 1"
+	done
+	if [ "$userIdActual" == "$1" ] ; then
+		return 0
+	else
+		return $encuestadorIdError
+	fi
+}
+
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Valida que la cantidad de respouestas de una encuesta determinada
+#                 coincida con la cantidad esperada.
+#
+# PRE-CONDICIÓN:  La variable 'lineas' contiene las preguntas de el archivo de
+#                 encuestas que se esta procesando.
+# POST-CONDICIÓN: Emite por stdout 0 si la cantidad de respuestas coincide con la
+#                 cantidad esperada. Un número mayor a 0 si hay más respuestas de las
+#                 esperadas o un número menor a 0 si hay menos.
+#
+# PARÁMETRO 1:    Número de línea desde donde empezar a contar las respuestas.
+# PARÁMETRO 2:    Cantidad de respuestas esperadas.
+# 
+# CÓDIGO ERROR:   0: Éxito.
+#======================================================================================
 validarCantidadPreguntas() {
 	aux="$1"
-	while [ "$aux" -le "${#lineas[@]}" -a `echo "${lineas[$aux]}" | grep -E -c "^D.*$"` -eq "1" ] ; do
+	while [ "$aux" -le "${#lineas[@]}" -a  \
+            `echo "${lineas[$aux]}" | grep -E -c "$regexDetalleSimple"` -eq "1" ] ; do
 		let "aux += 1"
 	done
 	echo "$(($aux - $1 - $2))"
+	return 0
 }
 
-# validarNumeroEncuesta <primer registro de detalle> <numero de encuesta>
-# Devuelve la cantidad de preguntas de esa encuesta
+#=== FUNCIÓN ==========================================================================
+# DESCRIPCIÓN:    Valida que todas las respuestas de un mismo bloque posean el mismo
+#                 número de encuesta.
+#
+# PRE-CONDICIÓN:  La variable 'lineas' contiene las preguntas de el archivo de
+#                 encuestas que se esta procesando.
+# POST-CONDICIÓN: Emite por stdout la cantidad de respuestas cuyo número de encuesta
+#                 coincidió con el número de encuesta recibido por parámetro.
+#
+# PARÁMETRO 1:    Número de línea desde donde empezar a contar las respuestas.
+# PARÁMETRO 2:    Número de encuesta.
+# 
+# CÓDIGO ERROR:   0: Éxito.
+#======================================================================================
 validarNumeroEncuesta() {
 	aux="$1"
-	while [ "$aux" -le "${#lineas[@]}" -a `echo "${lineas[$aux]}" | grep -E -c "^D${sc}$2${sc}.*$"` -eq "1" ] ; do
+	while [ "$aux" -le "${#lineas[@]}" -a \
+            `echo "${lineas[$aux]}" | grep -E -c "^D${sc}$2${sc}.*$"` -eq "1" ] ; do
 		let "aux += 1"
 	done
 	echo "$(($aux - $1))"
 }
+
+# TODO validar que no haya otra instancia corriendo
+
+paramsError=1
+pregIdError=18
+encuestaIdError=19
+encuestadorIdError=20
+
+
+origIfs=$IFS
+IFS=$'\n'
+
+grupo=.
+preguntas=($(cat "$grupo/mae/preguntas.mae"))
+encuestadores=($(cat "$grupo/mae/encuestadores.mae"))
+encuestas=($(cat "$grupo/mae/encuestas.mae"))
+archSumario="$grupo/grupo2/ya/encuestas.sum"
+archEncuestasRech="$grupo/grupo2/nolistos/encuestas.rech"
+sc=,												# Separador de campos
+
+: > "$archSumario" # TODO borrar
+: > "$archEncuestasRech" # TODO borrar
+: > "archivoDeLog" # TODO borrar
 
 # Creo el archivo encuestas.sum si no existe
 if [ ! -e $archSumario ] ; then
@@ -122,43 +266,68 @@ if [ ! -e $archEncuestasRech ] ; then
 	: > "$archEncuestasRech"
 fi
 
-if [ ! -e "unArchivo" ] ; then # TODO borrar, solo para testing
-	: > "unArchivo"
-fi
-
 dirIn=entrada
-regexCabecera="^C${sc}[0-9]+${sc}.{3}${sc}[0-9]*${sc}[PLESO]${sc}.+${sc}(ID|II|RP|RC)${sc}[ETCP]${sc}(ESP|MKT|VEN|LEG)${sc}.*$"
+regexEncuestador="^[0-9]{11}${sc}[^,]+${sc}[^,]{8}${sc}[0-9]{8}${sc}[0-9]{8}$"
+regexCabecera="^C${sc}[0-9]+${sc}.{3}${sc}[0-9]*${sc}[PLESO]${sc}.+${sc}(ID|II|RP|RC)\
+${sc}[ETCP]${sc}(ESP|MKT|VEN|LEG)${sc}.*$"
 regexDetalle="^D${sc}[0-9]+${sc}[0-9]+${sc}.+${sc}.*$"
+regexDetalleSimple="^D.*$"
+
+logInfo="I"
+logAlerta="A"
+logError="E"
+logFatal="SE"
 
 archivos=`ls "$grupo"/"$dirIn" | cat`
 
 for archivo in $archivos ; do
-	echo "Procesando Archivo: $archivo"
+
+	# TODO mover archivo
+	
+	logear $logInfo "Procesando archivo: $archivo"
 
 	userId=`echo "$archivo" | cut -d "." -f 1`
-	fechaEncuesta=`echo "$archivo" | cut -d "." -f 2`
+
+	validarUserId $userId
+	if [ "$?" -eq "$encuestadorIdError" ] ; then
+		logear $logError "User Id de encuestador incorrecto. Archivo \"$archivo\"\
+ rechazado"
+		cat "$grupo"/"$dirIn"/"$archivo" >> "$archEncuestasRech"
+		break
+	fi
+
+	fechaEncuesta=`echo "$archivo" | cut -d "." -f 2` # TODO validar que sea una fecha valida
 
 	lineas=($(cat "$grupo"/"$dirIn"/"$archivo"))
 	cantidadLineas=${#lineas[@]}
 
 	formatoCabeceraValido=0
-	nroLineaActual=-1
+	nroLineaActual=0
+	while [ "$nroLineaActual" -lt "$cantidadLineas" ] ; do
 
-	while [ "$nroLineaActual" -le "${#lineas[@]}" ] ; do
-		while [ "$formatoCabeceraValido" -ne "1" -a "$nroLineaActual" -le "$cantidadLineas" ] ; do
-			let "nroLineaActual += 1"
-			# TODO desechar bloque, log
-			formatoCabeceraValido=`echo "${lineas[$nroLineaActual]}" | grep -E -c "$regexCabecera"`
+		########################## Valido formato cabecera ############################
+		formatoCabeceraValido=0
+		while [ "$formatoCabeceraValido" -ne "1" -a \
+                "$nroLineaActual" -lt "$cantidadLineas" ] ; do
+
+			formatoCabeceraValido=\
+`echo "${lineas[$nroLineaActual]}" | grep -E -c "$regexCabecera"`
+
+			if [ "$formatoCabeceraValido" -ne "1" ] ; then
+				desecharBloque "$nroLineaActual" "nroLineaActual" \
+                                >> "$archEncuestasRech" 
+				logear $logError "Formato de cabecera incorrecto, rechazando encuesta. \
+Archivo \"$archivo\", línea número $nroLineaActual"
+			fi
 		done
 
-		if [ "$nroLineaActual" -gt "$cantidadLineas" ] ; then
-			# TODO log
+		if [ "$nroLineaActual" -ge "$cantidadLineas" ] ; then
+			logear $logAlerta "Se alcanzó el fin de archivo inesperadamente. \
+Archivo \"$archivo\""
 			break
 		fi
 
-		echo "Procesando cabecera: ${lineas[$nroLineaActual]}" >> "unArchivo"
-
-		############################ Parseo registro cabecera #############################
+		########################## Parseo registro cabecera ###########################
 		cabecera=`echo "${lineas[$nroLineaActual]}"`
 		nroEncuesta=`echo "$cabecera" | cut -d "$sc" -f 2`
 		codEncuesta=`echo "$cabecera" | cut -d "$sc" -f 3`
@@ -171,27 +340,33 @@ for archivo in $archivos ; do
 		inicioBloque=$nroLineaActual
 		desecharBloque="false"
 
-		################################## Validaciones ###################################
+		################################ Validaciones #################################
 		let "nroLineaActual += 1"
 		resultado=`validarCantidadPreguntas "$nroLineaActual" "$cantPregEsperadas"`
 		if [ "$resultado" -gt "0" ] ; then
-			echo "mas preguntas de las esperadas" >> "unArchivo"
+
+			logear $logError "Exceso de registro detalle. La encuesta número \
+\"$nroEncuesta\" perteneciente al archivo \"$archivo\" será rechazada"
 			desecharBloque="true"
+
 		elif [ "$resultado" -lt "0" ] ; then
-			echo "menos preguntas de las esperadas" >> "unArchivo"
+
+			logear $logError "Falta de registro detalle. La encuesta número \
+\"$nroEncuesta\" perteneciente al archivo \"$archivo\" será rechazada"
 			desecharBloque="true"
-		elif [ `validarNumeroEncuesta "$nroLineaActual" "$nroEncuesta"` -ne "$cantPregEsperadas" ] ; then
-			echo "numero de encuesta invalido" >> "unArchivo"
+
+		elif [ `validarNumeroEncuesta "$nroLineaActual" "$nroEncuesta"`\
+                -ne "$cantPregEsperadas" ] ; then
+
+			logear $logError "No todos los registros detalles de la encuesta \
+\"$nroEncuesta\" perteneciente al archivo \"$archivo\" tienen el mismo número de \
+encuesta. Dicha encuesta será rechazada"
 			desecharBloque="true"
+
 		else 	
-			############################# Calculo del puntaje #############################
+			########################### Calculo del puntaje ###########################
 			puntajeTotal="0"
 			while [ "$cantPregEsperadas" -gt "0" ] ; do
-				if [ $((`echo "${lineas[$nroLineaActual]}" | cut -d "$sc" -f 2`)) -ne "$nroEncuesta" ] ; then
-					# TODO Log error
-					desecharBloque="true"
-					break
-				fi
 	
 				pregId=`echo "${lineas[$nroLineaActual]}" | cut -d "$sc" -f 3`
 				respuesta=`echo "${lineas[$nroLineaActual]}" | cut -d "$sc" -f 4`
@@ -199,8 +374,10 @@ for archivo in $archivos ; do
 				puntajeParcial=`obtenerPuntaje "$pregId" "$respuesta"`
 
 				if [ $? -ne "0" ] ; then
-					# Log error
-					echo "Id de pregunta invalido $pregId" >> "unArchivo"	# TODO Borrar Linea
+
+					logear $logError "Id de pregunta invalido, el id: \"$pregId\" no \
+coincide con ninguna pregunta del archivo maestro. La encuesta número \
+\"$nroEncuesta\" perteneciente al archivo \"$archivo\" será rechazada"
 					desecharBloque="true"
 					break
 				fi
@@ -209,16 +386,16 @@ for archivo in $archivos ; do
 				let "nroLineaActual += 1"
 				let "cantPregEsperadas -= 1"
 			done
-			########################### Fin Calculo del puntaje ###########################
 		fi
 
 		if [ "$desecharBloque" = "true" ] ; then
-			echo "desecharBloque: $desecharBloque" >> "$archEncuestasRech"
+			desecharBloque "$inicioBloque" "nroLineaActual" >> "$archEncuestasRech"
 		else
-			echo "$userId${sc}$fechaEncuesta${sc}$nroEncuesta${sc}$codEncuesta${sc}$puntajeTotal${sc}$codCliente${sc}$sitioEncuesta${sc}$modEncuesta${sc}$personaEncuestada" >> "$archSumario"
+			echo "$userId${sc}$fechaEncuesta${sc}$nroEncuesta${sc}$codEncuesta${sc}\
+$puntajeTotal${sc}$codCliente${sc}$sitioEncuesta${sc}$modEncuesta${sc}\
+$personaEncuestada" >> "$archSumario"
 		fi
 		
-		let "nroLineaActual += 1"
 
 	done # Fin procesamiento del bloque
 	
