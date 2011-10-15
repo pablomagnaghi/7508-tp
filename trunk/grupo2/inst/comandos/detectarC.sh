6aa6dee623eb7d9cf1d62a9be280738d4961d53d
pany100 <pany100@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-15 22:41:02 +0000

    Cambio leves detalles del detectarC

--- trunk/grupo2/inst/comandos/detectarC.sh
+++ trunk/grupo2/inst/comandos/detectarC.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #Inicializo variables
-pid=$(ps | grep -m 1 detectarC.sh | awk '{ print $1 }');
+GRUPO="/home/luis/Escritorio/grupo2"
+ARRIDIR="$GRUPO/arribos"
 
-#Guardo el pid en el archivo .data.txt
-echo $pid > .data.txt;
+#Escribo el pid de la aplicacion en el archivo .data.txt
+pid=$(ps | grep -m 1 detectarC.sh | awk '{ print $1 }');
+echo $pid > $GRUPO/lib/.data.txt;
 
 
 #Si no exiten los directorios rechazados y preparados, los crea
@@ -15,19 +17,23 @@ if [ ! -e $GRUPO/preparados/ ];then
 	mkdir $GRUPO/preparados/;
 fi
 
-
+#Ciclo del demonio
 while [ -e .data.txt ] 
 do
 	cd $ARRIDIR;
 	for file in $(ls | grep -i '^[a-z]*\.[0-9]*$');
 	do
-        	userid=$(echo $file | cut -d "." -f 1);
+        
+		userid=$(echo $file | cut -d "." -f 1);
 	        idmatch=$(grep "^.*,.*,${userid}," encuestadores.mae);
         	if [ "$idmatch" == "" ]; then
+	
 			mv $file $GRUPO/rechazados/;
                	 	#loguear error
-        	else
-                	date=$(echo $file | cut -d "." -f 2);
+        
+		else
+        
+	        	date=$(echo $file | cut -d "." -f 2);
                 	begindate=$( echo ${idmatch} | cut -d "," -f 4);
                 	enddate=$( echo ${idmatch} | cut -d "," -f 5);
                 	if [ $begindate -le $date -a $enddate -ge $date ]; then
@@ -42,12 +48,12 @@ do
 	done
 	cd $GRUPO/preparados/;
 	arch=$(ls -a);
-	if [ arch != "" ]; then	
-		#sumarC
-	fi
+	#if [ arch != "" ]; then	
+	#	echo "iniciandosumarC"
+	#fi
 
 	#ir a preparados y hacer el sumar c
 	sleep 10;
 	cd $GRUPO;
+
 done	
-#sleep del demonio
