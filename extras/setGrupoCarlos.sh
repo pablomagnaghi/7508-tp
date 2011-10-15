10c2299d0524d2df9e3a3873502b01c57313bcbe
carlos.pantelides <carlos.pantelides@gmail.com>
2011-10-15 11:50:29 +0000

    listarc: adaptacion a nueva distribucion de carpetas

--- extras/setGrupoCarlos.sh
+++ extras/setGrupoCarlos.sh
@@ -1,5 +1,8 @@
 # copiar este archivo a setGrupoXXXX.sh y modificar la ruta
 # este script se ejecuta para inicializar el entorno en lugar de iniciard.sh
+
 export GRUPO=/home/carlos/7508/7508fiuba2011g2/trunk
-export DIRECTORIO_YA=/home/carlos/7508/7508fiuba2011g2/trunk/grupo2/ya
+export DIRECTORIO_LIB=$GRUPO/grupo2/lib
+export DIRECTORIO_MAESTROS=$GRUPO/grupo2/inst/mae
+export DIRECTORIO_YA=$GRUPO/extras/ya
 export ARCHIVO_ENCUESTAS=$DIRECTORIO_YA/encuestas.sum
