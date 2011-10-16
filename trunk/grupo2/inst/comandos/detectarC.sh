d2973ed5fa73e01e73c1937bcfb894babb0823b9
pany100 <pany100@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-16 02:19:58 +0000

    Detectar adaptado a la version actual. Falta rematarlo acordando con los muchachos

--- trunk/grupo2/inst/comandos/detectarC.sh
+++ trunk/grupo2/inst/comandos/detectarC.sh
@@ -3,56 +3,59 @@
 #Inicializo variables
 GRUPO="/home/luis/Escritorio/grupo2"
 ARRIDIR="$GRUPO/arribos"
+LIBDIR="lib"
+BINDIR="bin"
 
 #Escribo el pid de la aplicacion en el archivo .data.txt
 pid=$(ps | grep -m 1 detectarC.sh | awk '{ print $1 }');
 echo $pid > $GRUPO/lib/.data.txt;
 
-
-#Si no exiten los directorios rechazados y preparados, los crea
-if [ ! -e $GRUPO/rechazados/ ];then
-	mkdir $GRUPO/rechazados/;
-fi
-if [ ! -e $GRUPO/preparados/ ];then
-	mkdir $GRUPO/preparados/;
-fi
-
 #Ciclo del demonio
 while [ -e .data.txt ] 
 do
 	cd $ARRIDIR;
-	for file in $(ls | grep -i '^[a-z]*\.[0-9]*$');
+	for file in $(ls);
 	do
+        	if [ -f $file ];then
+
+			validName=$(echo $file | grep -i '^[a-z]*\.[0-9]*$')
+			if [ "$validName" != "" ];then
+			
+				userid=$(echo $file | cut -d "." -f 1);
+				idmatch=$(grep "^.*,.*,${userid}," $GRUPO/mae/encuestadores.mae);
+				if [ "$idmatch" == "" ]; then
+					./$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/;
+			       	 	./$GRUPO/loguearC.sh detectarC.sh A "No existe el usuario ${userid} en el archivo encuestadores, moviendo a rechazados"
+			
+				else
         
-		userid=$(echo $file | cut -d "." -f 1);
-	        idmatch=$(grep "^.*,.*,${userid}," encuestadores.mae);
-        	if [ "$idmatch" == "" ]; then
-	
-			mv $file $GRUPO/rechazados/;
-               	 	#loguear error
-        
+	        			date=$(echo $file | cut -d "." -f 2);
+                			begindate=$( echo ${idmatch} | cut -d "," -f 4);
+                			enddate=$( echo ${idmatch} | cut -d "," -f 5);
+                			if [ $begindate -le $date -a $enddate -ge $date ]; then
+						#moviendo a preparados
+						./$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/preparados/;
+				       	 	./$GRUPO/loguearC.sh detectarC.sh I "Moviendo ${file} a preparados"
+                			else
+						./$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/;
+				       	 	./$GRUPO/loguearC.sh detectarC.sh A "Fechas invalidas para ${userid}, moviendo a rechazados"
+                			fi
+        			fi
+			else
+				./$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/;
+		       	 	./$GRUPO/loguearC.sh detectarC.sh A "Nombre invalido para ${file}"
+			fi
 		else
-        
-	        	date=$(echo $file | cut -d "." -f 2);
-                	begindate=$( echo ${idmatch} | cut -d "," -f 4);
-                	enddate=$( echo ${idmatch} | cut -d "," -f 5);
-                	if [ $begindate -le $date -a $enddate -ge $date ]; then
-                        	#mover a preparados
-                        	mv $file $GRUPO/preparados/;
-                        	#loguear exito
-                	else
-				mv $file $GRUPO/rechazados/;
-                        	#loguear error
-                	fi
-        	fi
+			./$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/;
+	       	 	./$GRUPO/loguearC.sh detectarC.sh A "${file} no es un archivo regular"
+		fi
 	done
 	cd $GRUPO/preparados/;
 	arch=$(ls -a);
-	#if [ arch != "" ]; then	
-	#	echo "iniciandosumarC"
-	#fi
+	if [ arch != "" ]; then	
+		./$GRUPO/$BINDIR/sumarC.sh
+	fi
 
-	#ir a preparados y hacer el sumar c
 	sleep 10;
 	cd $GRUPO;
 
