3d17ae42abd0a0270ab1b304141f2165a6b5e2e5
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-17 13:05:57 +0000

    detectar: eliminacion variables innecesarias y correcion condicion de sumar. instalar: cambio minimo para que tome el chmod 755.

--- trunk/grupo2/inst/comandos/detectarC.sh
+++ trunk/grupo2/inst/comandos/detectarC.sh
@@ -2,8 +2,6 @@
 
 #Inicializo variables
 ARRIDIR="$GRUPO/arribos"
-LIBDIR="lib"
-BINDIR="bin"
 
 #Escribo el pid de la aplicacion en el archivo .data.txt
 pid=$(ps | grep -m 1 detectarC.sh | awk '{ print $1 }');
@@ -27,8 +25,8 @@ else
 					idmatch=$(grep "^.*,.*,${userid}," $GRUPO/mae/encuestadores.mae);
 					if [ "$idmatch" == "" ]; then
 						echo charly dice que no existe el usuario
-						$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/
-						$GRUPO/$LIBDIR/loguearC.sh detectarC.sh A "No existe el usuario ${userid} en el archivo encuestadores, moviendo a rechazados"
+						moverC.sh $file $GRUPO/rechazados/
+						loguearC.sh detectarC.sh A "No existe el usuario ${userid} en el archivo encuestadores, moviendo a rechazados"
 					else
 						date=$(echo $file | cut -d "." -f 2);
 						begindate=$( echo ${idmatch} | cut -d "," -f 4);
@@ -36,28 +34,28 @@ else
 						if [ $begindate -le $date -a $enddate -ge $date ]; then
 							echo charly dice que movera a preparados
 							#moviendo a preparados
-							$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/preparados/
-							$GRUPO/$LIBDIR/loguearC.sh detectarC.sh I "Moviendo ${file} a preparados"
+							moverC.sh $file $GRUPO/preparados/
+							loguearC.sh detectarC.sh I "Moviendo ${file} a preparados"
 						else
 							echo charly dice que mueve a rechazados por las fechas invalidas
-							$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/
-							$GRUPO/$LIBDIR/loguearC.sh detectarC.sh A "Fechas invalidas para ${userid}, moviendo a rechazados"
+							moverC.sh $file $GRUPO/rechazados/
+							loguearC.sh detectarC.sh A "Fechas invalidas para ${userid}, moviendo a rechazados"
 						fi
 					fi
 				else
 					echo charly dice que mueve a rechazados por el nombre invalido
-					$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/
-					$GRUPO/$LIBDIR/loguearC.sh detectarC.sh A "Nombre invalido para ${file}"
+					moverC.sh $file $GRUPO/rechazados/
+					loguearC.sh detectarC.sh A "Nombre invalido para ${file}"
 				fi
 			else
 				echo charly dice que mueve $file a rechazados pues no es un archivo regular, pero pienso que es mejor ignorarlo
-				$GRUPO/$LIBDIR/moverC.sh $file $GRUPO/rechazados/
-				$GRUPO/$LIBDIR/loguearC.sh detectarC.sh A "${file} no es un archivo regular"
+				moverC.sh $file $GRUPO/rechazados/
+				loguearC.sh detectarC.sh A "${file} no es un archivo regular"
 			fi
 		done
 		arch=$(ls $GRUPO/preparados )
 		echo charly dice que $arch
-		if [ $arch != "" ]; then
+		if [ "$arch" != "" ]; then
 			echo charly dice que vamos a sumar $arch
 			sumarC.sh
 		fi
