0263ccba9b0afedc47207982c4241dc4a043c4bc
carlos.pantelides <carlos.pantelides@gmail.com>
2011-10-17 02:34:18 +0000

    startd/stopd: deshardcodeo y desfuncionalizacion

--- grupo2/lib/startD.sh
+++ grupo2/lib/startD.sh
@@ -1,12 +1,12 @@
 #startD.sh
 
 
-	if [ -e data.txt ]; then
-		#agregar el log
-		echo "demonio corriendo";
-		sleep 3
-	else
-		echo "arrancando demonio $GRUPO/bin/detectarC.sh";
-		$GRUPO/bin/detectarC.sh &
-		sleep 3
-	fi;
+if [ -e  $GRUPO/lib/.data.txt ]; then
+	#agregar el log
+	echo "demonio corriendo";
+	sleep 3
+else
+	echo "arrancando demonio $GRUPO/bin/detectarC.sh";
+	$GRUPO/bin/detectarC.sh &
+	sleep 3
+fi;
--- grupo2/lib/stopD.sh
+++ grupo2/lib/stopD.sh
@@ -1,12 +1,11 @@
 #stopD
 
-stopD() {
-	if [ -e .data.txt ]; then
-		echo "parando demonio..";
-		rm .data.txt;
-		return 0
-	else
-		echo "demonio ya parado";
-		return 1
-	fi
-}
+if [ -e  $GRUPO/lib/.data.txt ]; then
+	echo "parando demonio..";
+	rm $GRUPO/lib/.data.txt;
+	return 0
+else
+	echo "demonio ya parado";
+	return 1
+fi
+
