2d8a9100a11e5b15584f3b921f0366fe26815c96
pany100 <pany100@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-18 01:37:27 +0000

    Detectar terminando

--- grupo2/inst/comandos/detectarC.sh
+++ grupo2/inst/comandos/detectarC.sh
@@ -1,65 +1,62 @@
 #!/bin/bash
 
-#Inicializo variables
-ARRIDIR="$GRUPO/arribos"
+ARCHIVO_CONF="$GRUPO/conf/instalarC.conf"
+
+. $ARCHIVO_CONF
 
 #Escribo el pid de la aplicacion en el archivo .data.txt
 pid=$(ps | grep -m 1 detectarC.sh | awk '{ print $1 }');
-echo $pid > $GRUPO/lib/.data.txt;
+echo $pid > $LIBDIR/.data.txt;
 if [ $? -ne 0 ]; then
-	echo No se pudo crear archivo testigo
+	loguearC.sh detectarC SE "No se pudo crear el archivo testigo para ejecutar detectarC.sh"
 else 
-	echo charly dice que esta todo bien
+	loguearC.sh detectarC I "Arrancando demonio"
 
 	#Ciclo del demonio
-	while [ -e $GRUPO/lib/.data.txt ]; do
-		echo charly dice que hacemos otro loop en $ARRIDIR
+	while [ -e $LIBDIR/.data.txt ]; do
+
 		cd $ARRIDIR
+
 		for file in $(ls $ARRIDIR ); do
-			echo charly dice que va a ver que hacer con $file
+
 			if [ -f $file ];then
 				validName=$(echo $file | grep -i '^[a-z]*\.[0-9]*$')
+
 				if [ "$validName" != "" ];then
-					echo charly dice que hay un nombre valido
 					userid=$(echo $file | cut -d "." -f 1);
-					idmatch=$(grep "^.*,.*,${userid}," $GRUPO/mae/encuestadores.mae);
+					idmatch=$(grep "^.*,.*,${userid}," $DATAMAE/encuestadores.mae);
+
 					if [ "$idmatch" == "" ]; then
-						echo charly dice que no existe el usuario
 						moverC.sh $file $GRUPO/rechazados/
-						loguearC.sh detectarC.sh A "No existe el usuario ${userid} en el archivo encuestadores, moviendo a rechazados"
+						loguearC.sh detectarC A "No existe el usuario ${userid} en el archivo encuestadores, moviendo a rechazados"
 					else
 						date=$(echo $file | cut -d "." -f 2);
 						begindate=$( echo ${idmatch} | cut -d "," -f 4);
 						enddate=$( echo ${idmatch} | cut -d "," -f 5);
+
 						if [ $begindate -le $date -a $enddate -ge $date ]; then
-							echo charly dice que movera a preparados
-							#moviendo a preparados
 							moverC.sh $file $GRUPO/preparados/
-							loguearC.sh detectarC.sh I "Moviendo ${file} a preparados"
+							loguearC.sh detectarC I "Moviendo ${file} a preparados"
 						else
-							echo charly dice que mueve a rechazados por las fechas invalidas
 							moverC.sh $file $GRUPO/rechazados/
-							loguearC.sh detectarC.sh A "Fechas invalidas para ${userid}, moviendo a rechazados"
+							loguearC.sh detectarC A "Fechas invalidas para ${userid}, moviendo a rechazados"
 						fi
 					fi
 				else
-					echo charly dice que mueve a rechazados por el nombre invalido
 					moverC.sh $file $GRUPO/rechazados/
-					loguearC.sh detectarC.sh A "Nombre invalido para ${file}"
+					loguearC.sh detectarC A "Nombre invalido para ${file}"
 				fi
 			else
-				echo charly dice que mueve $file a rechazados pues no es un archivo regular, pero pienso que es mejor ignorarlo
 				moverC.sh $file $GRUPO/rechazados/
-				loguearC.sh detectarC.sh A "${file} no es un archivo regular"
+				loguearC.sh detectarC A "${file} no es un archivo regular"
 			fi
 		done
 		arch=$(ls $GRUPO/preparados )
-		echo charly dice que $arch
 		if [ "$arch" != "" ]; then
-			echo charly dice que vamos a sumar $arch
+			loguearC.sh detectarC I "Invocando sumarC"
 			sumarC.sh
 		fi
 		sleep 10;
 	done
-	echo "saliendo de detectar"	
+	loguearC.sh detectarC I "Terminando detectar.."
 fi
