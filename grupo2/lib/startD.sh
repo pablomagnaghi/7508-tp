79558b77aee6422dcc26779c3bf7a46965f91157
pany100 <pany100@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-18 02:09:28 +0000

    sdas

--- grupo2/lib/startD.sh
+++ grupo2/lib/startD.sh
@@ -7,7 +7,7 @@ ARCHIVO_CONF="$GRUPO/conf/instalarC.conf"
 . $ARCHIVO_CONF
 
 if [ -e  $LIBDIR/.data.txt ]; then
-	loguearC.sh startD A "El demonio ya esta incializado"
+	loguearC.sh startD A "El demonio ya esta inicializado"
 	sleep 3
 else
 	loguearC.sh startD I "Arrancando demonio"
