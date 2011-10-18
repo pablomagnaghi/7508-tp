9edd34df9d81b1da55d86be0634b8c29ff8b4e36
pany100 <pany100@7b75ecd5-4871-96cf-3a74-b83c63b06bb8>
2011-10-18 00:32:04 +0000

    StartD y StopD listos

--- grupo2/lib/startD.sh
+++ grupo2/lib/startD.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 #startD.sh
 
+ARCHIVO_CONF="$GRUPO/conf/instalarC.conf"
 
-if [ -e  $GRUPO/lib/.data.txt ]; then
-	#agregar el log
-	echo "demonio corriendo";
+. $ARCHIVO_CONF
+
+if [ -e  $LIBDIR/.data.txt ]; then
+	loguearC.sh startD A "El demonio ya esta incializado"
 	sleep 3
 else
-	echo "arrancando demonio $GRUPO/bin/detectarC.sh";
-	$GRUPO/bin/detectarC.sh &
+	loguearC.sh startD I "Arrancando demonio"
+	$BINDIR/detectarC.sh &
 	sleep 3
 fi;
