47d7c863e93e164afff0d74873e57f5001ac9349
carlos.pantelides <carlos.pantelides@gmail.com>
2011-10-18 19:20:00 +0000

    iniciar: correccion de exits y deteccion de source

--- grupo2/inst/comandos/iniciarC.sh
+++ grupo2/inst/comandos/iniciarC.sh
@@ -8,22 +8,22 @@
 #Si la cantidad de parámetros, exceptuando al cero, es mayor a cero
 #muestro un mensaje de error
 
-
-if [ $0 != 'bash' ]; then
+echo $0 | grep -q -e 'bash'
+if [ $? -ne 0  ]; then
 	echo "Este comando debe invocarse como \". iniciarC.sh\""
 	exit 1
 fi
 
 if [ $# -gt 0 ]; then
 	echo "No pueden existir parametros" >&2
-	exit 1
+	return 1
 fi
 
 #VERIFICO LA EXISTENCIA DEL ARCHIVO DE CONFIGURACION
 
 if [ -z $GRUPO ]; then
 	echo "Inicialización de ambiente no fue exitosa. No Ha sido seteada la variable \$GRUPO"
-	exit 1
+	return 1
 fi
 
 
@@ -31,7 +31,7 @@ ARCHIVO_CONF="$GRUPO/conf/instalarC.conf"
 
 if [ ! -f $ARCHIVO_CONF ]; then
 	echo "Inicialización de ambiente no fue exitosa. No existe archivo de configuración $ARCHIVO_CONF"
-	exit 1
+	return 1
 fi
 
 #SETEO INICIAL DE LAS VARIABLES DE AMBIENTE
@@ -77,7 +77,7 @@ DIRECTORIOS=( RECHAZADOS PREPARADOS LISTOS NOLISTOS YA )
 for i in "${DIRECTORIOS[@]}";do
 	if [ ! -d $GRUPO${!i} ]; then
 		echo "Inicialización de ambiente no fue exitosa. No existe directorio ${GRUPO}${!i}"
-		exit 1
+		return 1
 	fi
 done
 
@@ -87,7 +87,7 @@ DIRECTORIOS=( CONFDIR DATAMAE LIBDIR ARRIDIR LOGDIR BINDIR )
 for i in "${DIRECTORIOS[@]}";do
 	if [ ! -d ${!i} ]; then
 		echo "Inicialización de ambiente no fue exitosa. No existe directorio ${GRUPO}${!i}"
-		exit 1
+		return 1
 	fi
 done
 
@@ -102,7 +102,7 @@ ARCHIVOS=( DETECTAR SUMAR LISTAR )
 for i in "${ARCHIVOS[@]}";do
 	if [ ! -x ${BINDIR}/${!i} ]; then
 		echo "Inicialización de ambiente no fue exitosa. No existe archivo ${BINDIR}/${!i} o no tiene permiso de ejecución"
-		exit 1
+		return 1
 	fi
 done
 
@@ -119,7 +119,7 @@ ARCHIVOS=( LOGUEAR MIRAR MOVER START STOP )
 for i in "${ARCHIVOS[@]}";do
 	if [ ! -x ${LIBDIR}/${!i} ]; then
 		echo "Inicialización de ambiente no fue exitosa. No existe archivo ${LIBDIR}/${!i} o no tiene permiso de ejecución"
-		exit 1
+		return 1
 	fi
 done
 
@@ -138,7 +138,7 @@ for i in "${ARCHIVOS[@]}";do
 	if [ ! -r ${DATAMAE}/${!i} ]; then
 		$LOGUEAR iniciarC SE "Inicialización de ambiente no fue exitosa. No existe archivo ${$DATAMAE}/${!i} o no tiene permiso de lectura"
 		echo "Inicialización de ambiente no fue exitosa. No existe archivo ${$DATAMAE}/${!i} o no tiene permiso de lectura"
-		exit 1
+		return 1
 	fi
 done
 
@@ -162,7 +162,7 @@ if [ -r $LIBDIR/$ARCHIVO_PID ]; then
 	if [ $? -eq 0 ]; then
 		$LOGUEAR iniciarC SE "Inicialización de ambiente no fue exitosa. El comando $DETECTAR se encuentra corriendo"
 		echo "Inicialización de ambiente no fue exitosa. El comando $DETECTAR se encuentra corriendo"
-		exit 1
+		return 1
 	fi
 	rm $LIBDIR/$ARCHIVO_PID
 fi
@@ -178,7 +178,7 @@ sleep 2
 if [ $r -ne 0 ]; then
 	$LOGUEAR iniciarC SE "Inicialización de ambiente no fue exitosa. Error al ejecutar el comando ${START}"
 	echo "Inicialización de ambiente no fue exitosa. Error al ejecutar el comando ${START}"
-	#exit 1
+	return 1
 else
 	#Busco el número de pid en el archivo data.txt
 	#Hipotesis: este archivo esta en la carpeta actual si startD.sh
@@ -186,7 +186,7 @@ else
 	if [ ! -r $LIBDIR/$ARCHIVO_PID ]; then
 		$LOGUEAR iniciarC SE "Inicialización de ambiente no fue exitosa. No existe archivo $LIBDIR/${ARCHIVO_PID} o no tiene permiso de lectura"
 		echo "Inicialización de ambiente no fue exitosa. No existe archivo $LIBDIR/${ARCHIVO_PID} o no tiene permiso de lectura"
-		#exit 1
+		return 1
 	else
 		PID=$(cat $LIBDIR/$ARCHIVO_PID)	
 	fi
